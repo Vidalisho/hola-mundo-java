492d93057519c6e5511b056782250195c0ed355b
Vidalisho <estabanserrano@hotmail.com>
2026-08-26 10:04:24 -0800

    Modificacion para mostrar mi nombre

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo{
     public static void main(String[] args){
         System.out.println("hola mundo :p");
+        System.out.println("Esteban Vidal Serrano Gonzalez");
     }
 }
